a92af8653a89a20dd0e97dd7f4c0e060bed98568
DannyBaine-Entity <apololadanieltolu@gmail.com>
2024-03-06 11:22:36 +0100

    Changing the links

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('portfolio.html')
+
+app.route('/portfolio', methods=['GET'])
+def portfolio():
+    return render_template('portfolio.html')
+
+app.route('/experience', methods=['GET'])
+def experience():
+    return render_template('experience.html')
+
+app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+app.route('/skills', methods=['GET'])
+def skills():
+    return render_template('skills.html')
+
+@app.route('/education', methods=['GET'])
+def education():
+    return render_template('education.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- portfolio.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, redirect, url_for
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('portfolio.html')
